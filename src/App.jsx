08fb23b54e741a6dccda38f53004bf3e35af2529
Rajendraprasad <RajendraPrasad.Jeevanantham@ftr.com>
2025-07-02 00:59:05 +0530

    : Initial Post-Processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { EffectComposer, N8AO } from '@react-three/postprocessing'
+import { useState } from 'react'
 import './App.css'
 
-function Scene() {
+function Scene({ showAO }) {
   return (
     <>
       <ambientLight intensity={0.5} />
@@ -11,16 +13,25 @@ function Scene() {
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="orange" />
       </mesh>
+      {showAO && (
+        <EffectComposer>
+          <N8AO intensity={2} aoRadius={2} />
+        </EffectComposer>
+      )}
       <OrbitControls />
     </>
   )
 }
 
 function App() {
+  const [showAO, setShowAO] = useState(true)
   return (
     <div className="fullscreen-canvas">
+      <button style={{position:'absolute',zIndex:1,top:20,left:20}} onClick={() => setShowAO(v => !v)}>
+        {showAO ? 'Disable AO' : 'Enable AO'}
+      </button>
       <Canvas>
-        <Scene />
+        <Scene showAO={showAO} />
       </Canvas>
     </div>
   )
